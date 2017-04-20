2877bc99106a9ba8a31ac0eae2b57a5110abeef9
thescs <gthescs@gmail.com>
2017-04-20 12:33:44 +0300

    Client-side loader
    Client-side loader for Tampermonkey (Chromium-based) & Greasemonkey (Firefox) created.

---
+++ loader.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name        MagicTreeLoader
+// @namespace   thescs.naumen.sd.displayhiddenblocks.loader
+// @description Shows hidden areas in Service Desk (autoupdate)
+// @include     https://sd.volia.com:8443/fx/voliagwt/*
+// @version     2
+// @grant       none
+// ==/UserScript==
+
+function include(src, callback)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+
+    if(callback)
+    {
+        var userAgent = navigator.userAgent.toLowerCase();
+        
+        if(/msie/.test( userAgent ) && !/opera/.test( userAgent ))
+        {
+            script.onreadystatechange = function()
+            {
+                if(script.readyState == 'complete')
+                    callback();
+            }
+        }
+        else
+        {
+            script.onload = function ()
+            {
+                callback();
+            }
+        }
+    }
+
+    script.src = src;
+    head.appendChild(script);
+}
+
+include('https://rawgit.com/thescs/sdMagicTree/autoupdate/miracle.js');
